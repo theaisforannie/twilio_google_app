33bbbf56882526af5f5feb22d4fe8e9330879940
theaisforannie <annharter@gmail.com>
2015-06-15 18:30:20 -0400

    aw geez now there is a database. this commit makes it look easy. it took hours and steven is the nicest.

--- app.rb
+++ app.rb
@@ -2,6 +2,70 @@ require 'rubygems'
 require 'twilio-ruby'
 require 'sinatra'
 require 'yaml'
+require 'pg'
+require 'uri'
+
+host = "localhost"
+port = 5432
+dbname = "test_db"
+user = nil
+password = nil
+table_name = ENV['TABLE_NAME'] || "test_table"
+
+if ENV['DATABASE_URL'] != nil
+	uri = URI.parse(ENV['DATABASE_URL'])
+	host = uri.hostname
+	port = uri.port
+	dbname = uri.path[1..-1]
+	user = uri.user
+	password = uri.password
+	puts ":D"
+end
+
+conn = PG.connect(host, port, nil, nil, dbname, user, password)
+
+
+def save_params(params)
+	"INSERT INTO #{table_name} (
+		accountsid,
+		apiversion,
+		body,
+		\"From\",
+		fromcity,
+		fromcountry,
+		fromstate,
+		fromzip,
+		messagesid,
+		nummedia,
+		smsmessagesid,
+		smssid,
+		smsstatus,
+		\"To\",
+		tocity,
+		tocountry,
+		tostate,
+		tozip
+		) VALUES (
+		\'#{params["AccountSid"]}\',
+		\'#{params["ApiVersion"]}\',
+		\'#{params["Body"]}\',
+		\'#{params["From"]}\',
+		\'#{params["FromCity"]}\',
+		\'#{params["FromCountry"]}\',
+		\'#{params["FromState"]}\',
+		\'#{params["FromZip"]}\',
+		\'#{params["MessageSid"]}\',
+		\'#{params["NumMedia"]}\',
+		\'#{params["SmsMessageSid"]}\',
+		\'#{params["SmsSid"]}\',
+		\'#{params["SmsStatus"]}\',
+		\'#{params["To"]}\',
+		\'#{params["ToCity"]}\',
+		\'#{params["ToCountry"]}\',
+		\'#{params["ToState"]}\',
+		\'#{params["ToZip"]}\'
+		);"
+end	
 
 def msg(str)
 	if str == nil || str.strip == ""
@@ -16,7 +80,9 @@ def msg(str)
 end
 
 get '/' do
+	conn.exec save_params(params)
 	str = params['Body'].downcase
+	
 	puts msg(str)
 
 	twiml = Twilio::TwiML::Response.new do |r|
